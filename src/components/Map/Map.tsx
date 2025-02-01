fd026daae74ebfddd204f1185406b0bf278cad51
Vitaly Kindrakevich <vitaly@kindrakevich.com>
2025-02-01 09:08:21 +0100

    Add markers border

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -38,7 +38,7 @@ const getItemIcon = (pin: Pin, active: boolean) => {
     case 'Вершини':
       return L.divIcon({
         className: className,
-        html: `<div class="w-full h-full rounded-full text-white flex items-center justify-center" style="background-color: ${RED}; box-shadow: 0 0 8px 0 ${RED};"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mountain"><path d="m8 3 4 8 5-5 5 15H2L8 3z"/></svg></div>`,
+        html: `<div class="w-full h-full rounded-full text-white flex items-center justify-center" style="background-color: ${RED}; border: 1px solid #FFF;"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mountain"><path d="m8 3 4 8 5-5 5 15H2L8 3z"/></svg></div>`,
         iconSize: [22, 22]
       });
   }
@@ -46,13 +46,13 @@ const getItemIcon = (pin: Pin, active: boolean) => {
     case 'Проживання':
       return L.divIcon({
         className: className,
-        html: `<div class="w-full h-full rounded-full text-white flex items-center justify-center" style="background-color: ${BLUE}; box-shadow: 0 0 8px 0 ${BLUE};"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg></div>`,
+        html: `<div class="w-full h-full rounded-full text-white flex items-center justify-center" style="background-color: ${BLUE}; border: 1px solid #FFF;"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg></div>`,
         iconSize: [22, 22]
       });
     default:
       return L.divIcon({
         className: className,
-        html: `<div class="w-full h-full rounded-full text-white flex items-center justify-center" style="background-color: ${GREEN}; box-shadow: 0 0 8px 0 ${GREEN};"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-landmark"><line x1="3" x2="21" y1="22" y2="22"/><line x1="6" x2="6" y1="18" y2="11"/><line x1="10" x2="10" y1="18" y2="11"/><line x1="14" x2="14" y1="18" y2="11"/><line x1="18" x2="18" y1="18" y2="11"/><polygon points="12 2 20 7 4 7"/></svg></div>`,
+        html: `<div class="w-full h-full rounded-full text-white flex items-center justify-center" style="background-color: ${GREEN}; border: 1px solid #FFF;"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-landmark"><line x1="3" x2="21" y1="22" y2="22"/><line x1="6" x2="6" y1="18" y2="11"/><line x1="10" x2="10" y1="18" y2="11"/><line x1="14" x2="14" y1="18" y2="11"/><line x1="18" x2="18" y1="18" y2="11"/><polygon points="12 2 20 7 4 7"/></svg></div>`,
         iconSize: [22, 22]
       });
   }
